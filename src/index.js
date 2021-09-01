0a4cf0d2ef1d6fa64102c3f2f546033b4c3dd2a5
celestialheathen <jingchaoyu@hotmail.com>
2021-09-01 18:17:20 -0400

    Using map to loop through array to create components

--- src/index.js
+++ src/index.js
@@ -2,34 +2,45 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import './index.css' 
 
+const cars = [
+  {
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/2019_Tesla_Model_3_Performance_AWD_Front.jpg/560px-2019_Tesla_Model_3_Performance_AWD_Front.jpg",
+    name: "Model 3",
+    price: 38000
+  },
+  
+  {
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/2017_Tesla_Model_X_100D_Front.jpg/560px-2017_Tesla_Model_X_100D_Front.jpg",
+    name: "Model X",
+    price: 120000
+  }
+
+]
+
+
 function CarList() {
   return (
     <section className="carlist">
-      <Car />
-      <Car />
-      <Car />
-      <Car />
-      <Car />
+      {cars.map( (car) => {
+        const {img, name, price} = car 
+        return (
+          <Car img={img} nane={name} price={price} />
+        )
+      })
+      }
     </section>
   )
 }
 
-const Car = () => {
+
+const Car = ( {img, name, price} ) => {
   return (
     <article className="car">
-      <Image/>
-      <Name/>
-      <Price/>
+     <img src={img} alt=""/>
+     <h1>{name}</h1>
+     <h4>{price}</h4>
     </article>
   )
 }
-
-const Image = () => <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/2019_Tesla_Model_3_Performance_AWD_Front.jpg/560px-2019_Tesla_Model_3_Performance_AWD_Front.jpg"/>
-
-const Name = () => <h1>Model 3</h1>
-
-const Price = () => (
-  <h4 style={ {color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem'} }>$38000</h4>
-)
 
 ReactDom.render(<CarList/>, document.getElementById('root'))
