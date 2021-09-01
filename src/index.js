e8d7cc73aafb957d5bb228dfa910c0939fd05a4d
celestialheathen <jingchaoyu@hotmail.com>
2021-09-01 16:52:58 -0400

    added styles

--- src/index.js
+++ src/index.js
@@ -1,19 +1,35 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import './index.css' 
 
-function BookList() {
+function CarList() {
   return (
-    <section>
-      <Book />
+    <section className="carlist">
+      <Car />
+      <Car />
+      <Car />
+      <Car />
+      <Car />
     </section>
   )
 }
 
-const Book = () => {
-  return <article>
-    This is a book
-  </article>
+const Car = () => {
+  return (
+    <article className="car">
+      <Image/>
+      <Name/>
+      <Price/>
+    </article>
+  )
 }
 
+const Image = () => <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/2019_Tesla_Model_3_Performance_AWD_Front.jpg/560px-2019_Tesla_Model_3_Performance_AWD_Front.jpg"/>
 
-ReactDom.render(<BookList/>, document.getElementById('root'))
+const Name = () => <h1>Model 3</h1>
+
+const Price = () => (
+  <h4 style={ {color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem'} }>$38000</h4>
+)
+
+ReactDom.render(<CarList/>, document.getElementById('root'))
